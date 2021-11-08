a36ea3a7be70041582060a4e35d9531794df0e23
904154001 <904154001@L829WD150074.edu.cbe.ab.ca>
2021-11-08 10:12:57 -0700

    not working

--- RecursiveSearcher/RecursiveSearcher/src/RecursiveSearcher.java
+++ RecursiveSearcher/RecursiveSearcher/src/RecursiveSearcher.java
@@ -1,10 +1,25 @@
 
 public class RecursiveSearcher {
 	
-	public RecursiveSearcher() {
-	}
+	public int RecursiveSearcher() {
 
 	public int doBinarySearch(IntegerArray arrayOfInts, int target) {
+		 return dobinarySearch(target, 0, arrayOfInts.length()-1);
 	}
-	
+	}
+	private int dobinarySearch(Object target, int lo, int hi){
+		   if(lo > hi) {
+		      return -1;
+		   } else {
+		      int mid = (lo+hi)/2;
+		      int comp = vec[mid].compareTo(target);
+		      if(comp == 0){
+		          return mid;
+		      } else if(comp < 0){
+		          return dobinarySearch(target, mid+1, hi);
+		      } else {
+		          return dobinarySearch(target, lo, mid-1);
+		   }
+		}
 }
+	}
